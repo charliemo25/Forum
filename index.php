0af9372f3e3be57e74562150e28e8479ac8f4a39
Moreau Charlie <moreaucharlie25@gmail.com>
2022-02-10 14:33:23 +0100

    Récupération des posts

--- index.php
+++ index.php
@@ -10,15 +10,21 @@ $router = new Router($_SERVER["REQUEST_URI"]);
 $router->get('/', function(){
     echo "HomePage";
 });
-$router->get('/posts', function(){
-    echo "Tous les articles";
-});
-$router->get('/posts/:id-:slug', function($id, $slug) use($router){
-    echo $router->url('Posts#show', ["id" => 1, "slug" => "salut-les-gens"]);
-}, 'post.show')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
-$router->get('/posts/:id', "Posts#show");
-$router->post('/posts/:id', function($id){
-    echo "Poster pour l'article $id <pre>".print_r($_POST, true)."</pre>";
-});
+
+$router->get('/posts', "Post#index");
+
+// $router->get('/posts', function(){
+//     echo "Tous les articles";
+// });
+
+// $router->get('/posts/:id-:slug', function($id, $slug) use($router){
+//     echo $router->url('Posts#show', ["id" => 1, "slug" => "salut-les-gens"]);
+// }, 'post.show')->with('id', '[0-9]+')->with('slug', '[a-z\-0-9]+');
+
+// $router->get('/posts/:id', "Posts#show");
+
+// $router->post('/posts/:id', function($id){
+//     echo "Poster pour l'article $id <pre>".print_r($_POST, true)."</pre>";
+// });
 
 $router->run();
---
+++ src/Controller/PostController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use PDO;
+use App\PDO\Connexion;
+use App\Repository\PostRepository;
+use App\Repository\UserRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\RoleRepository;
+
+class PostController {
+
+    private PDO $pdo;
+    private PostRepository $postRepository;
+    private RoleRepository $roleRepository;
+    private UserRepository $userRepository;
+    private CategoryRepository $categoryRepository;
+
+    public function __construct()
+    {
+        $this->pdo = (new Connexion())->getPdo();
+        $this->roleRepository = new RoleRepository($this->pdo);
+        $this->userRepository = new UserRepository($this->pdo, $this->roleRepository);
+        $this->categoryRepository = new CategoryRepository($this->pdo);
+        $this->postRepository = new PostRepository($this->pdo, $this->userRepository, $this->categoryRepository);
+    }
+
+    public function index(){
+        // Récuperer la liste des posts
+        print_r($this->postRepository->findAll());
+        // echo "Homepage des posts";
+    }
+    
+}
